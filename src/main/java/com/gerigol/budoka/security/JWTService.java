849d9375b70656feb6999e55b85237fb689381df
gerigol <golengerhard@gmail.com>
2023-07-13 16:59:48 +0200

    feat: implement token generator

--- src/main/java/com/gerigol/budoka/security/JWTService.java
+++ src/main/java/com/gerigol/budoka/security/JWTService.java
@@ -7,16 +7,39 @@ import lombok.RequiredArgsConstructor;
 
 import java.security.Key;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import io.jsonwebtoken.*;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class JWTService {
     private static final String SECRET = System.getenv("JWT_SECRET");
+    private static final long JWT_EXPIRATION = 86400000;//24H
 
+    public String generateToken(Authentication authentication) {
+        return generateToken(new HashMap<>(), authentication);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            Authentication authentication
+    ) {
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + JWT_EXPIRATION);
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .setClaims(extraClaims)
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(getSignInKey())
+                .compact();
+    }
 
     public String getUsernameFromToken(String token) {
         return extractClaim(token, Claims::getSubject);
