a00dbf2d2904d5889d9cdb1374114c46562c1281
gerigol <golengerhard@gmail.com>
2023-07-18 10:51:55 +0200

    feat: create registration endpoint

---
+++ src/main/java/com/gerigol/budoka/controller/restcontroller/RegisterController.java
@@ -0,0 +1,9 @@
+package com.gerigol.budoka.controller.restcontroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class RegisterController {
+}
