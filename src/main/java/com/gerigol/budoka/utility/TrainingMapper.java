27552b72dc3e42347db23ed2d7747a6173ab516f
gerigol <golengerhard@gmail.com>
2023-09-05 17:14:27 +0200

    feat: create mapper class for Training

---
+++ src/main/java/com/gerigol/budoka/utility/TrainingMapper.java
@@ -0,0 +1,16 @@
+package com.gerigol.budoka.utility;
+
+import com.gerigol.budoka.controller.dto.TrainingDTO;
+import com.gerigol.budoka.domain.Training;
+
+public class TrainingMapper {
+    public static TrainingDTO toDTO(Training training) {
+        return new TrainingDTO(training.getPublicId(),
+                training.getName(),
+                training.getStartDateTime(),
+                training.getEndDateTime(),
+                training.getLocation(),
+                training.getTrainers(),
+                training.getTrainees());
+    }
+}
